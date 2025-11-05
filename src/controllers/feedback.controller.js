baa6a7ed9dafe861321a95799f7be4570f68021e
shashanke2 <149947774+shashanke2@users.noreply.github.com>
2025-11-05 13:00:44 +0530

    Update feedback.controller.js

--- src/controllers/feedback.controller.js
+++ src/controllers/feedback.controller.js
@@ -2,14 +2,15 @@ import Feedback from "../models/feedback.model.js";
 
 export const submitFeedback = async (req, res) => {
   try {
-    const { bookingId, userEmail, stars, review } = req.body;
+    const { bookingId, providerId, userEmail, stars, review } = req.body;
 
-    if (!bookingId  || !userEmail || !stars) {
+    if (!bookingId || !providerId || !userEmail || !stars) {
       return res.status(400).json({ message: "Missing required fields." });
     }
 
     const feedback = await Feedback.create({
       bookingId,
+      providerId,  // store provider id
       userEmail,
       stars,
       review,
@@ -20,16 +21,6 @@ export const submitFeedback = async (req, res) => {
       message: "Feedback submitted successfully!",
       feedback,
     });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-export const getProviderFeedback = async (req, res) => {
-  try {
-    const { providerId } = req.params;
-    const feedbacks = await Feedback.find({ providerId });
-    res.json({ success: true, feedbacks });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
